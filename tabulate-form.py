b07f3b4cf01c310aefa71ca9a48bb4f7d4284593
Kamil Janeček <j@necek.com>
2020-01-22 13:25:28 +0100

    fixup

--- tabulate-form.py
+++ tabulate-form.py
@@ -6,7 +6,7 @@ import re
 import sys
 
 reader = csv.reader(sys.stdin)
-data = [i for i in reader][1:-3]
+data = [i for i in reader][1:]
 
 res = defaultdict(list)
 for i in data:
@@ -18,7 +18,4 @@ for i in data:
 
 
 cols = [[i] + res[i] for i in res]
-
-max_cnt = max((len(i) for i in cols))
-
 print(tabulate(itertools.zip_longest(*cols, fillvalue='')))
