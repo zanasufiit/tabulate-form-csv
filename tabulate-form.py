53713008ccfb0e5f26ff6a2ddfa5b4a4b464eba2
Kamil Janeček <j@necek.com>
2020-01-23 01:20:53 +0100

    update

--- tabulate-form.py
+++ tabulate-form.py
@@ -5,17 +5,27 @@ import csv
 import re
 import sys
 
+times_regex = '(\d+\.\d+\.\d+, \d+:\d+, \w+)'
+
 reader = csv.reader(sys.stdin)
 data = [i for i in reader][1:]
 
 res = defaultdict(list)
+
+times = set()
+
+for i in data:
+    for w in re.findall(times_regex, i[2]):
+        times.add(w)
+
+times = list(times).sort()
+
 for i in data:
     who = i[1]
-    when = re.findall('(\d+\.\d+\.\d+, \d+:\d+, \w+)', i[2])
+    when = re.findall(times_regex, i[2])
     
-    for w in when:
-        res[w].append(who)
-
+    for w in times:
+        res[w].append(who if w in when else '')
 
 cols = [[i] + res[i] for i in res]
 print(tabulate(itertools.zip_longest(*cols, fillvalue='')))
